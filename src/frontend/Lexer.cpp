d096e279a4244719dfac57d10dcab312b95665f2
blazejOz <blazej.ozog@gmail.com>
2026-03-18 13:43:21 +0100

    feat(lexer): implement token recognition for EBNF v2

--- src/frontend/Lexer.cpp
+++ src/frontend/Lexer.cpp
@@ -68,12 +68,50 @@ Token Lexer::getNextToken()
         return {TokenType::NUMBER, num};
     }
 
-    // Handle Single Characters ( ) ;
+    //Handle Comparison Operators
     char c = advance();
+    
+    if(c == '='){
+        if(peek() == '='){
+            advance();
+            return {TokenType::EQ, "=="};
+        }
+    }
+    
+    if(c == '!'){
+        if(peek() == '='){
+            advance();
+            return {TokenType::NOT_EQ, "!="};
+        }
+    }
+    
+    if(c == '>'){
+        if(peek() == '='){
+            advance();
+            return {TokenType::GE, ">="};
+        }else{
+            return {TokenType::GT, ">"};
+        }
+    }
+    
+    if(c == '<'){
+        if(peek() == '='){
+            advance();
+            return {TokenType::LE, "<="};
+        }else{
+            return {TokenType::LT, "<"};
+        }
+    }
+    
+    // Handle Single Characters
+    if (c == '{') return {TokenType::L_CURL, "{"};
+    if (c == '}') return {TokenType::R_CURL, "}"};
     if (c == '(') return {TokenType::L_PAREN, "("};
     if (c == ')') return {TokenType::R_PAREN, ")"};
     if (c == ';') return {TokenType::SEMICOLON, ";"};
     if (c == '=') return {TokenType::ASSIGN, "="};
+    if (c == ':') return {TokenType::COLON, ":"};
+    if (c == ',') return {TokenType::COMMA, ","};
 
     //ARITHMETIC
     if (c == '+') return {TokenType::ADD, "+"};
